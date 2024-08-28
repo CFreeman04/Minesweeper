8d2a68e3651b81c16b7041bea8a41103780a3bc2
CFreeman04 <charlie.e.freeman@gmail.com>
2024-08-28 15:59:07 -0500

    Deleted unnecessary files

--- obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f310b2af2c5a934aee0e727fa2094a0fe750a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
