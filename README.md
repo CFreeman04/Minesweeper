f6ee6879cae9e8929372285c3a310c52fb15768c
CFreeman04 <charlie.e.freeman@gmail.com>
2024-08-28 16:25:13 -0500

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Minesweeper
+
+This is the classic Minesweeper game impleted in Windows Form C#
+
+## Screens
+![Initial](Screens/Initial.jpg)
+
+![Win](Screens/Win.jpg)
+
+![Selected Bomb](<Screens/Selected Bomb.jpg>)
+
+![Wrong Flag](<Screens/Wrong Flag.jpg>)
+
--- obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f310b2af2c5a934aee0e727fa2094a0fe750a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d2a68e3651b81c16b7041bea8a41103780a3bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
